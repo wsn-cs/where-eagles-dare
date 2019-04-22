356c414031a17fcf4ff5e81b6e0b53f623964657
gabrielmerlin <47850060+gabrielmerlin@users.noreply.github.com>
2019-04-22 19:35:13 +0200

    Ajout de la définition de schur_partition_struct

--- MonteCarloMethod/schurNumberNestedMonteCarloHeader.h
+++ MonteCarloMethod/schurNumberNestedMonteCarloHeader.h
@@ -13,6 +13,26 @@
 #include <stdlib.h>
 #include <gmp.h>
 
+#include <stdlib.h>
+#include <gmp.h>
+
+struct schur_partition_struct {
+    unsigned int pmax;  // Nombre maximal de huches
+    unsigned int p;     // Nombre de huches non vides
+    
+    unsigned long n;    // Entier courant
+    mp_size_t limballoc;// Nombre de limbes alloués à chaque huche
+    mp_size_t limbsize; // Nombre de limbes utilisés par chaque huche
+    
+    mp_limb_t **partition;
+    mp_limb_t **partitioninvert;
+    
+    mp_limb_t *work0;
+    mp_limb_t *work1;
+};
+
+typedef struct schur_partition_struct partition_t;
+
 /*Fonction ajoutant les entiers un par un*/
 unsigned long schurNumberSimpleMonteCarloLevelIteration(partition_t *sfpartitionstruc, unsigned int level,
   mp_limb_t **sfpartitionbest, unsigned int *pbestptr, unsigned int iternum);
