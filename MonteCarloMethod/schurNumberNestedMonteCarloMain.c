09f9e4fd6b7e8e067fe36e20312273534f00baa5
gabrielmerlin <47850060+gabrielmerlin@users.noreply.github.com>
2019-05-01 15:26:38 +0200

    Implémentation de asprintf
    Sous Windows, la fonction asprintf n'est pas implémentée. J'ignore si ce code fonctionne.

--- MonteCarloMethod/schurNumberNestedMonteCarloMain.c
+++ MonteCarloMethod/schurNumberNestedMonteCarloMain.c
@@ -51,6 +51,41 @@ void printPartition(unsigned long p, unsigned long n, mp_limb_t **partition) {
     }
 }
 
+/*Dans le cas où la fonction asprintf n'est pas implémentée, par exemple sous Windows.*/
+
+#ifndef _vscprintf
+/* For some reason, MSVC fails to honour this #ifndef. */
+/* Hence function renamed to _vscprintf_so(). */
+int _vscprintf_so(const char * format, va_list pargs) {
+    int retval;
+    va_list argcopy;
+    va_copy(argcopy, pargs);
+    retval = vsnprintf(NULL, 0, format, argcopy);
+    va_end(argcopy);
+    return retval;}
+#endif // _vscprintf
+
+#ifndef vasprintf
+int vasprintf(char **strp, const char *fmt, va_list ap) {
+    int len = _vscprintf_so(fmt, ap);
+    if (len == -1) return -1;
+    char *str = malloc((size_t) len + 1);
+    if (!str) return -1;
+    int r = vsnprintf(str, len + 1, fmt, ap); /* "secure" version of vsprintf */
+    if (r == -1) return free(str), -1;
+    *strp = str;
+    return r;}
+#endif // vasprintf
+
+#ifndef asprintf
+int asprintf(char *strp[], const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    int r = vasprintf(strp, fmt, ap);
+    va_end(ap);
+    return r;}
+#endif // asprintf
+
 int main(int argc, const char * argv[]) {
     char optc;
     unsigned int p, i;
